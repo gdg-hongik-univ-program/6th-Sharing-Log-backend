d432588b3a01662aacfd28add19140fe8f2f127c
Lim <sangugim664@naver.com>
2026-07-07 19:25:57 +0900

    sharingLog 프로젝트 코드 반영

---
+++ src/main/java/gdg/sharinglog/SharingLogApplication.java
@@ -0,0 +1,13 @@
+package gdg.sharinglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharingLogApplication.class, args);
+    }
+
+}
---
+++ src/main/java/gdg/sharinglog/TestController.java
@@ -0,0 +1,6 @@
+package gdg.sharinglog;
+
+public class TestController {
+    String test; // 테스트
+    int a = 1; // 테스트 테스트 테스트
+}
