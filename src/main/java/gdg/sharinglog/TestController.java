e24da6af2880b6233e54ccf847892f6947dbf3df
Lim <sangugim664@naver.com>
2026-07-07 19:43:17 +0900

    7/7

--- src/main/java/gdg/sharinglog/TestController.java
+++
@@ -1,6 +0,0 @@
-package gdg.sharinglog;
-
-public class TestController {
-    String test; // 테스트
-    int a = 1; // 테스트 테스트 테스트
-}
---
+++ src/main/java/gdg/sharinglog/TestControllerTwo.java
@@ -0,0 +1,5 @@
+package gdg.sharinglog;
+
+public class TestControllerTwo {
+    String string = "마지막 테스트";
+}
